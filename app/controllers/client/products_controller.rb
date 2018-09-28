2cae684c5b8a17141f60cee03d3fdb3963d99b07
sbudisav <sbudisav@gmail.com>
2018-09-27 21:36:08 -0500

    adds more restful routes

--- app/controllers/client/products_controller.rb
+++ app/controllers/client/products_controller.rb
@@ -6,4 +6,28 @@ class Client::ProductsController < ApplicationController
     @products = responce.parse
     render 'index.html.erb'
   end
+
+  def new
+    render 'new.html.erb'
+  end
+
+  def create
+    client_params = {name: params["name"],
+                     price: params["price"],
+                    image_url: params["image_url"],
+                    description: params["description"],
+                    stock: params["stock"],
+                    supplier_id: params["supplier_id"]
+                    }
+    responce = HTTP.post("http://localhost:3000/api/products", form: client_params)
+
+    @product = responce.parse
+    redirect_to "/client/products/#{@product["id"]}"
+  end
+
+  def show
+    responce = HTTP.get("http://localhost:3000/api/products/#{params[:id]}")
+    @product = responce.parse
+    render 'show.html.erb'
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-  # STEP 1: A ROUTE triggers a controller action
-  # verb "/urls" => "namespace/controllers#action"
-
   namespace :client do
     get '/products' => 'products#index'
+    get '/products/new' => 'products#new'
+    post '/products' => 'products#create'
+    get '/products/:id' => 'products#show'
+    get '/products/:id/edit' => 'products#edit'
   end
 end
